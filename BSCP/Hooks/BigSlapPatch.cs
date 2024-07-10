a980a023aa24514586d04502a31f67d8a225aebd
AnthonySirois <anthonysirois999@gmail.com>
2024-07-10 13:25:16 -0400

    add flag nerf or warn+freeze

--- BSCP/Hooks/BigSlapPatch.cs
+++ BSCP/Hooks/BigSlapPatch.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx.Logging;
+using DefaultNamespace;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ namespace BSCP.Patches
 {
     public class BigSlapPatch
     {
-        private const float CONTAINMENT_TIME_IN_SECONDS = 10f;
+        private const float CONTAINMENT_TIME_IN_SECONDS = 5f;
 
         private static Dictionary<Bot_BigSlap, float> containedMonsters = new Dictionary<Bot_BigSlap, float>();
         private static ManualLogSource? Logger;
@@ -14,26 +15,72 @@ namespace BSCP.Patches
         internal static void Init(ManualLogSource logger)
         {
             Logger = logger;
-            /*
-             *  Subscribe with 'On.Namespace.Type.Method += CustomMethod;' for each method you're patching.
-             *  Or if you are writing an ILHook, use 'IL.' instead of 'On.'
-             *  Note that not all types are in a namespace, especially in Unity games.
-             */
-            On.Bot_BigSlap.Start += BigSlap_Start;
-            On.Bot_BigSlap.ComputeState += BigSlap_ComputeState;
+            
+            if (false)
+            {
+                On.Bot_BigSlap.Start += BigSlap_Start_WarnAndFreeze;
+                On.Bot_BigSlap.ComputeState += BigSlap_ComputeState_WarnAndFreeze;
+            } else
+            {
+                On.Bot_BigSlap.Start += BigSlap_Start_Nerf;
+            }
         }
 
-        private static void BigSlap_Start(On.Bot_BigSlap.orig_Start orig, Bot_BigSlap self)
+        private static void BigSlap_Start_Nerf(On.Bot_BigSlap.orig_Start orig, Bot_BigSlap self)
         {
             orig(self);
 
-            // Destory the BigSlap when spawned
-            // MonoBehaviour.Destroy(self.transform.parent.gameObject);
-
-            containedMonsters.TryAdd(self, Time.time + CONTAINMENT_TIME_IN_SECONDS);
+            GameObject root = self.transform.parent.gameObject;
+            PlayerController botController = root.GetComponent<PlayerController>();
+            if (botController != null)
+            {
+                botController.sprintMultiplier = 3.5f;
+                botController.movementForce = 10f;
+            } else
+            {
+                Logger?.LogError("Failed to find BigSlap.PlayerController");
+            }
         }
 
-        private static void BigSlap_ComputeState(On.Bot_BigSlap.orig_ComputeState orig, Bot_BigSlap self)
+        private static void BigSlap_Start_WarnAndFreeze(On.Bot_BigSlap.orig_Start orig, Bot_BigSlap self)
+        {
+            orig(self);
+
+            containedMonsters.TryAdd(self, Time.time + CONTAINMENT_TIME_IN_SECONDS);
+
+            PlayWarningSound(self);
+        }
+
+        private static void PlayWarningSound(Bot_BigSlap self)
+        {
+            Transform root = self.transform.parent;
+            SFX_PlayOneShot[] sfxs = root.gameObject.GetComponentsInChildren<SFX_PlayOneShot>();
+            foreach (var sfx in sfxs)
+            {
+                if (sfx.gameObject.name == "VoiceNotice")
+                {
+                    Vector3 playerPosition = Player.localPlayer.HeadPosition();
+                    Vector3 direction = (root.position - playerPosition).normalized;
+                    Vector3 soundPosition = playerPosition + 5 * direction;
+
+                    PlaySFX(sfx, soundPosition);
+                }
+            }
+        }
+
+        private static void PlaySFX(SFX_PlayOneShot sfx, Vector3 position)
+        {
+            if (sfx.sfx)
+            {
+                SFX_Player.instance.PlaySFX(sfx.sfx, position, null, null, 1f, false, false, true, 0f, 0);
+            }
+            foreach (var childSfx in sfx.sfxs)
+            {
+                SFX_Player.instance.PlaySFX(childSfx, position, null, null, 1f, false, false, true, 0f, 0);
+            }
+        }
+
+        private static void BigSlap_ComputeState_WarnAndFreeze(On.Bot_BigSlap.orig_ComputeState orig, Bot_BigSlap self)
         {
             if (containedMonsters.TryGetValue(self, out float containmentEndTime))
             {
